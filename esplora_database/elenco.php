a6d59695df997d1281236e9272ddae89b46394ab
Grumeno <150341402+Grumeno@users.noreply.github.com>
2023-12-20 09:26:17 +0100

    Update elenco.php

--- esplora_database/elenco.php
+++ esplora_database/elenco.php
@@ -1,27 +1,40 @@
 <?php
- include("parametri.php");
 
- $connect = mysqli_connect($server, $username, $password)
-  or die("Connessione non riuscita: " . mysqli_error($connect));
- // print("Connesso con successo <br>");
+  include("parametri.php");
+  
+  // Connessione al server dbms
+  $connect = mysqli_connect($server, $username, $password, $databases)
+        or die("Connessione non riuscita: " . mysqli_error($connect));
+  print ("Connesso con successo <br />");
+  
+  // selezione database
+  mysqli_select_db($connect, $database) 
+    or die ("Impossibile selezionare il db");
+  
+    // Esecuzione della query
+  $query = "SHOW TABLES"; // query da eseguire
+  $result = mysqli_query($connect, $query)
+    or die ("Errore nella query" . mysqli_error($connect));
+  
+  // Visualizzo il risultato della query
+  if (!$result){
+    echo "Errore nella query: ".mysqli_error($connect);
+  
+    }else{
 
- mysqli_select_db($connect, $database)
-  or die("Impossibile selezionare il db");
+    echo "<h2>Elenco delle tabelle presenti </h2>";
+    echo "<table border = 1>";   
 
- $query = "SHOW TABLES";
- $result = mysqli_query($connect, $query)
-  or die("Errore nella query" . mysqli_error($connect));
+    while ($search = mysqli_fetch_array($result))
+    {
+      echo "<tr><th><a href = 'struttura.php?table={$search[0]}'>" . $search[0] . "</a></th></tr>";
+    }
 
- echo "<html><head><title>Elenco Tabelle</title></head></body>";
- echo "<h1>Elenco delle Tabelle</h1>";
- echo "<ul>";
+    echo "</table>";
+    mysqli_free_result($result);
 
- while($table = mysqli_fetch_array($result)) {
-  echo "<li><a href = 'struttura.php?table={$table[0]}'>" . $table[0] . "</a></li>";
- }
-
- echo "</ul></body></html>";
-
- mysqli_free_result($result);
- mysqli_close($connect);
+  }
+  
+  // libero la memoria occupata dall'istruzione SELECT
+  mysqli_close($connect); // chiusura della connessione al server Mysql
 ?>
