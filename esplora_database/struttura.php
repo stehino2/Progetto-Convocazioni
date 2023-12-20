3b8b919b83da554a5afa772e08675ff0790556ef
Grumeno <150341402+Grumeno@users.noreply.github.com>
2023-12-20 09:26:36 +0100

    Update struttura.php

--- esplora_database/struttura.php
+++ esplora_database/struttura.php
@@ -3,15 +3,14 @@
 
  $connect = mysqli_connect($server, $username, $password)
   or die("Connessione non riuscita: " . mysqli_error($connect));
- // print("Connesso con successo <br>");
 
  mysqli_select_db($connect, $database)
   or die("Impossibile selezionare il db");
 
  $selected_table = $_GET['table']; // ottengo il nome della tabella
 
- $query = "DESCRIBE " . $selected_table;
- $result = mysqli_query($connect, $query)
+ $query = "DESCRIBE " . $selected_table; //comando vien usato per descrivere la struttura della tabella
+ $result = mysqli_query($connect, $query) // viene utilizzato per mandare una query in SQL in questo caso DESCRIBE
   or die("Errore nella query" . mysqli_error($connect));
 
  echo "<html><head><title>Struttura della Tabella</title></head><body>";
@@ -24,8 +23,11 @@
   echo "<tr><td>{$row['Field']}</td><td>{$row['Type']}</td></tr>";
  }
 
+ //Il collegamento ipertestuale, quando cliccato, reindirizza l'utente alla pagina "contenuto.php" con un parametro
+ // "table" uguale al valore della variabile "$selected_table". Questo parametro verrà utilizzato nella pagina "contenuto.php"
+ // per ottenere il contenuto della tabella selezionata.
  echo "</table><p><a href='contenuto.php?table=$selected_table'>Visualizza Contenuto</a></p></body></html>";
 
  mysqli_free_result($result);
  mysqli_close($connect);
-?>
+?>  
