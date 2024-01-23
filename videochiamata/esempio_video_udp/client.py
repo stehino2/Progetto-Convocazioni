bec2a181d75bac366ce8ed78d0f8fc722b22e668
stehino2 <150341032+stehino2@users.noreply.github.com>
2024-01-23 08:27:58 +0100

    Add files via upload

---
+++ videochiamata/esempio_video_udp/client.py
@@ -0,0 +1,32 @@
+import cv2
+import socket
+import os
+
+def main():
+    #Family address IPV4   //UDP Protocol, send different datagrams, limited dimension
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = '127.0.0.1'  
+    port = 11500
+
+    cap = cv2.VideoCapture(0)  #  #0 = video camera ; path_to_video
+    #cap = capture
+    #cv2 = library to handle images
+    
+    while cap.isOpened():
+        #ret = boolean value; frame is the image
+        ret, frame = cap.read()
+
+        #The frame can be bigger than the buffer size. To avoid that we resize it. 
+        frame = cv2.resize(frame, (640, 480))
+        #encoded is a bool
+        encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        if len(buffer) > 65536:#max dim of the buffer
+            print("The frame is too large to be sent over UDP")
+        else:
+            client_socket.sendto(buffer, (host, port))#send the data
+
+    cap.release()#end the capture
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
---
+++ videochiamata/server/server.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import socket
+import threading
+
+def main():
+    #udp socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = '127.0.0.1'
+    port = 11500
+    buffer_size = 65536 #max datagram dimension
+
+    server_socket.bind((host, port))
+    print(f"UDP server listening on {host}:{port}...")
+    #Like tkinter
+    cv2.namedWindow("Streaming Video", cv2.WINDOW_NORMAL)
+
+
+    while True:
+        try:
+            packet, _ = server_socket.recvfrom(buffer_size)
+            print("Packet received")
+            #np = numpy = library to handle multidimensional array
+            data = np.frombuffer(packet, dtype=np.uint8) #datatype of the buffer = 8bytes integer (unsigned long integer)
+            frame = cv2.imdecode(data, cv2.IMREAD_COLOR) #convert bytes to image
+            if frame is not None:
+                cv2.imshow("Streaming Video", frame)#show image, Streaming Video window name
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+
+    server_socket.close()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
